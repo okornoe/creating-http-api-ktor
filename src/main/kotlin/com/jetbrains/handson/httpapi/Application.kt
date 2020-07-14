f7a2e57236b2a54621eb294634ed346cbf41e524
Kelvin Okornoe <kelvin.okornoe@gmail.com>
2020-07-14 21:37:43 +0000

    remove

--- src/main/kotlin/com/jetbrains/handson/httpapi/Application.kt
+++ src/main/kotlin/com/jetbrains/handson/httpapi/Application.kt
@@ -2,7 +2,6 @@ package com.jetbrains.handson.httpapi
 
 import io.ktor.application.Application
 
-
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 fun Application.module() {
